9ffa0590fb2bb7fd74c538d8991196393b4f98a6
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2021-07-29 09:42:55 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+eureka_plotter_car.auto_led_off(microbit_LED.無効)
+eureka_plotter_car.microbit_version_info(microbit_version.Version2)
+plotLED_blocks.neopixel_rainbow()
+eureka_plotter_car.plottercar_pen(pen_updown.up)
 basic.forever(function () {
 	
 })
